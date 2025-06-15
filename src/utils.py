e6561e64145ef681aa3e8a625ace1f0c50f164ed
Satyam255 <satyambindhani25@gmail.com>
2025-06-15 22:47:38 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     """
@@ -25,3 +26,31 @@ def save_object(file_path,obj):
            dill.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys) 
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    """
+    Evaluates multiple regression models and returns a report of their performance.
+    
+    Parameters:
+    - X_train: Training feature set.
+    - y_train: Training target variable.
+    - X_test: Testing feature set.
+    - y_test: Testing target variable.
+    - models (dict): Dictionary of model names and their instances.
+    
+    Returns:
+    - model_report (dict): Dictionary containing model names and their R2 scores.
+    """
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            train_model_score = r2_score(y_train, y_train_pred)
+            y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
