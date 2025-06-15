7e303e90207053943f67c78896563f6f693b8856
Satyam255 <satyambindhani25@gmail.com>
2025-06-15 23:08:24 +0530

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
     """
     Saves an object to a specified file path using numpy's save function.
@@ -27,30 +27,34 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e, sys) 
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
-    """
-    Evaluates multiple regression models and returns a report of their performance.
     
-    Parameters:
-    - X_train: Training feature set.
-    - y_train: Training target variable.
-    - X_test: Testing feature set.
-    - y_test: Testing target variable.
-    - models (dict): Dictionary of model names and their instances.
-    
-    Returns:
-    - model_report (dict): Dictionary containing model names and their R2 scores.
-    """
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
+
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
             y_train_pred = model.predict(X_train)
-            train_model_score = r2_score(y_train, y_train_pred)
+
             y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
+
             report[list(models.keys())[i]] = test_model_score
+
         return report
+
     except Exception as e:
         raise CustomException(e, sys)
